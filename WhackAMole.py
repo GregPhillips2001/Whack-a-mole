239b5ba86b251493dae2ac9ed24a7a6c6079261e
GregPhillips2001 <31485934+GregPhillips2001@users.noreply.github.com>
2017-11-01 13:56:08 -0400

    Updated from Brython Server: 11/1/2017 1:56:07 PM

--- WhackAMole.py
+++ WhackAMole.py
@@ -11,8 +11,13 @@ if __name__ == '__main__':
     
     red = Color(0xFF0000,1)
     black = Color(0x000000,1)
+    white = Color(0xFFFFFF,1)
     
+    blackCircle = CircleAsset(50,LineStyle(1,black), white)
     redCircle = CircleAsset(50,LineStyle(1,red),red) #(radius,outline,fill)
+    
+    
+    Sprite(blackCircle)
 
-    App().run(step)
+    App().run()
 
