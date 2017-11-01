873904cfd30286dfbe9b1d18a6906a1ddb7b284c
GregPhillips2001 <31485934+GregPhillips2001@users.noreply.github.com>
2017-11-01 13:48:37 -0400

    Updated from Brython Server: 11/1/2017 1:48:36 PM

--- WhackAMole.py
+++ WhackAMole.py
@@ -1,1 +1,11 @@
+#Greg Phillips
+#11/1/17
+#WhackAMole.py - Computer programming project whack-a-mole program
+
+from ggame import *
+from random import randint
+
+if __name__ == '__main__': 
+
+App().run(step)
 
