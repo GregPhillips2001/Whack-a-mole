11aeb600c75f5bafe3851e937f2a135d292620ee
GregPhillips2001 <31485934+GregPhillips2001@users.noreply.github.com>
2017-11-02 14:32:44 -0400

    Updated from Brython Server: 11/2/2017 2:32:43 PM

--- WhackAMole.py
+++ WhackAMole.py
@@ -5,6 +5,19 @@
 from ggame import *
 from random import randint
 
+def mouseClick(event):
+    if event.x >= 0 and event.x <= 100 and event.y >= 0 and event.y <= 100:
+        print("Circle 1")
+    if event.x >= 101 and event.x <= 200 and event.y >= 0 and event.y <= 100:
+        print("Circle 2")
+    if event.x >= 201 and event.x <= 300 and event.y >= 0 and event.y <= 100:
+        print("Circle 3")
+    if event.x >= 0 and event.x <= 100 and event.y >= 101 and event.y <= 200:
+        print("Circle 4")
+    if event.x >= 101 and event.x <= 200 and event.y >= 101 and event.y <= 200:
+        print("Circle 5")
+    if event.x >= 201 and event.x <= 300 and event.y >= 101 and event.y <= 200:
+        print("Circle 6")
 
 
 if __name__ == '__main__': 
@@ -17,12 +30,12 @@ if __name__ == '__main__':
     
     
     #Creates circles where mole can appear
-    circle1 = Sprite(blackCircle,(50,50))
+    circle1 = Sprite(blackCircle,(50,50)) #(num of pixels right, num of pixels down)
     circle2 = Sprite(blackCircle,(150,50))
-    cirlcle3 = Sprite(blackCircle,(250,50))
-    Sprite(blackCircle,(50,150))
-    Sprite(blackCircle,(150,150))
-    Sprite(blackCircle,(250,150))
+    circle3 = Sprite(blackCircle,(250,50))
+    circle4 = Sprite(blackCircle,(50,150))
+    circle5 = Sprite(blackCircle,(150,150))
+    circle6 = Sprite(blackCircle,(250,150))
     
     
     App().listenMouseEvent("click", mouseClick) #Listens for mouse click
